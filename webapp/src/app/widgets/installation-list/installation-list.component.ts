277cee20dfb36c3d160538bead1c4287c0d74d98
Andrea Maschio <andrea.maschio@gmail.com>
2018-10-05 09:21:23 +0200

    sort of working angular login

---
+++ webapp/src/app/widgets/installation-list/installation-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-installation-list',
+  templateUrl: './installation-list.component.html',
+  styleUrls: ['./installation-list.component.css']
+})
+export class InstallationListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
