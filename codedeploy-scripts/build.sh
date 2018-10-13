f39dd907f2060e7dadff471b5ab19912d5a29d30
Andrea Maschio <andrea.maschio@gmail.com>
2018-10-13 16:45:44 +0200

    some fixes

--- codedeploy-scripts/build.sh
+++ codedeploy-scripts/build.sh
@@ -4,8 +4,8 @@ cp /home/ec2-user/wicloud-local/local.py .
 ln -s local.py settings.py
 sudo chown -R ec2-user:ec2-user /var/www/html/wicloud/
 cd /var/www/html/wicloud
-virtualenv django --python /usr/bin/python3
-. venv/bin/activate
-pip install .
+virtualenv django --python /usr/local/bin/python3
+. django/bin/activate
+pip3 install .
 dj migrate
 deactivate
