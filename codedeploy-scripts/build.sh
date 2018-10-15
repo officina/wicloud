1d39c730f0206ec44a845ea600ae71cc13465f61
Andrea Maschio <andrea.maschio@gmail.com>
2018-10-15 15:52:30 +0200

    fix

--- codedeploy-scripts/build.sh
+++ codedeploy-scripts/build.sh
@@ -3,7 +3,8 @@ source ~/.bash_profile
 cd /var/www/html/wicloud/web/settings
 cp /home/ec2-user/wicloud-local/local.py .
 ln -sf local.py settings.py
-sudo chown -R !(logs/*) ec2-user:ec2-user /var/www/html/wicloud/
+sudo chown -R !(logs) ec2-user:ec2-user /var/www/html/wicloud/
+sudo chown apache:apache /var/www/html/wicloud/logs/*
 cd /var/www/html/wicloud
 /usr/local/bin/virtualenv django --python /usr/local/bin/python3
 . django/bin/activate
