132aa3986eaf1a62ffaf3679478ebcebbce91d51
Andrea Maschio <andrea.maschio@gmail.com>
2018-10-15 15:57:10 +0200

    fix + explanations

--- codedeploy-scripts/build.sh
+++ codedeploy-scripts/build.sh
@@ -1,20 +1,33 @@
 #!/bin/sh
+# read all exported variables and initialize nvm
 source ~/.bash_profile
+# change working folder
 cd /var/www/html/wicloud/web/settings
+# copy the setting file
 cp /home/ec2-user/wicloud-local/local.py .
+# symobolic link overriding existing link
 ln -sf local.py settings.py
-sudo chown -R !(logs) ec2-user:ec2-user /var/www/html/wicloud/
+# change permissions to the main prject folder..
+sudo chown -R ec2-user:ec2-user /var/www/html/wicloud/
+# .. excluded the log folders
 sudo chown apache:apache /var/www/html/wicloud/logs/*
+# back to the main project folder
 cd /var/www/html/wicloud
+# initialize virtualenv
 /usr/local/bin/virtualenv django --python /usr/local/bin/python3
+# activate virtualenv
 . django/bin/activate
+# package instalaltion
 pip3 install .
+# db migrations
 dj migrate
+# deactivate virtualenv
 deactivate
 #frontend
 cd /var/www/html/wicloud/webapp
 npm install
 webpack
+# after building the frontend, upload to s3
 dj collectstatic --noinput
 
 
