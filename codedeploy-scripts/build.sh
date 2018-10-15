85ac2007500bbb582464ef8d85cea2f9d35ec5c4
Andrea Maschio <andrea.maschio@gmail.com>
2018-10-15 15:46:41 +0200

    fix symbolic link broken

--- codedeploy-scripts/build.sh
+++ codedeploy-scripts/build.sh
@@ -2,7 +2,7 @@
 source ~/.bash_profile
 cd /var/www/html/wicloud/web/settings
 cp /home/ec2-user/wicloud-local/local.py .
-ln -s local.py settings.py
+ln -sf local.py settings.py
 sudo chown -R !(logs/*) ec2-user:ec2-user /var/www/html/wicloud/
 cd /var/www/html/wicloud
 /usr/local/bin/virtualenv django --python /usr/local/bin/python3
