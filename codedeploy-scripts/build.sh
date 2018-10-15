0b8fe2a2f09a0738391f0e526f9c4a1f9f25547a
Andrea Maschio <andrea.maschio@gmail.com>
2018-10-15 15:16:45 +0200

    inversione webpack e collectstatic

--- codedeploy-scripts/build.sh
+++ codedeploy-scripts/build.sh
@@ -3,16 +3,17 @@ source ~/.bash_profile
 cd /var/www/html/wicloud/web/settings
 cp /home/ec2-user/wicloud-local/local.py .
 ln -s local.py settings.py
-sudo chown -R ec2-user:ec2-user /var/www/html/wicloud/
+sudo chown -R !(logs/*) ec2-user:ec2-user /var/www/html/wicloud/
 cd /var/www/html/wicloud
 /usr/local/bin/virtualenv django --python /usr/local/bin/python3
 . django/bin/activate
 pip3 install .
 dj migrate
-dj collectstatic --noinput
 deactivate
+#frontend
 cd /var/www/html/wicloud/webapp
 npm install
 webpack
+dj collectstatic --noinput
 
 
