ac5e4aa52cb60516de2c88bdfc939ad7a03808f9
Andrea Maschio <andrea.maschio@gmail.com>
2018-10-16 12:29:13 +0200

    fix

--- codedeploy-scripts/build.sh
+++ codedeploy-scripts/build.sh
@@ -27,6 +27,7 @@ cd /var/www/html/wicloud/webapp
 npm install
 NODE_ENV=production webpack --mode=production
 # after building the frontend, upload to s3 the different files (see Collectfast plugin for django-storage)
+cd /var/www/html/wicloud/
 dj collectstatic --noinput
 
 # deactivate virtualenv
