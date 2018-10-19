e8cb60fd09d1eed3a3a3df83356039213e208a00
Andrea Maschio <andrea.maschio@gmail.com>
2018-10-19 17:06:24 +0200

    JWT to Bearer

--- web/settings/base.py
+++ web/settings/base.py
@@ -238,6 +238,7 @@ CORS_ORIGIN_WHITELIST = [
 JWT_AUTH = {
     'JWT_EXPIRATION_DELTA': datetime.timedelta(days=1),
     'JWT_RESPONSE_PAYLOAD_HANDLER': 'web.core.serializers.jwt_response_payload_handler',
+    'JWT_AUTH_HEADER_PREFIX' : 'Bearer',
 }
 
 
@@ -256,4 +257,5 @@ AUTHENTICATION_BACKENDS =['django.contrib.auth.backends.ModelBackend', 'web.core
 # ACCOUNT_AUTHENTICATION_METHOD = 'email'
 # ACCOUNT_EMAIL_REQUIRED = True
 # ACCOUNT_USERNAME_REQUIRED = False
+
 
