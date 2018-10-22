94aa438501c2d7adfd1296a4a2e700809e5b71ea
Andrea Maschio <andrea.maschio@gmail.com>
2018-10-22 18:38:17 +0200

    pipeline change

---
+++ angular/codebuild-scripts/postbuild.sh
@@ -0,0 +1,7 @@
+# Clear S3 bucket.
+cd angular
+aws s3 rm s3://${S3_BUCKET} --recursive
+echo S3 bucket is cleared.
+Copy dist folder to S3 bucket, As of Angular 6, builds are stored inside an app folder in distribution and not at the root of the dist folder
+aws s3 cp dist s3://${S3_BUCKET} --recursive
+echo Build completed on `date`
