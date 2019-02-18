32f2f756204bca8323ccbb3d21304bef7ca612ce
andrea.toppan@wi4b.it <bologna123>
2019-02-18 17:34:07 +0100

    * angular, fix to directives before pushing

--- angular/src/app/widgets/widgets.module.ts
+++ angular/src/app/widgets/widgets.module.ts
@@ -9,7 +9,7 @@ import {NodeListGridWidgetComponent} from './node-list-grid-widget/node-list-gri
 import {NodeSmallWidgetComponent} from './node-small-widget/node-small-widget.component';
 import {NodeSmallDetailsWidgetComponent} from './node-small-details-widget/node-small-details-widget.component';
 import {FlotGraphComponent} from '../shared/flot/flot.component';
-import {NgSparklineDirective} from '../shared/_directives/ngSparkline.directive';
+import {NgSparklineDirective} from './_directives/ngSparkline.directive';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {MatButtonModule, MatCheckboxModule, MatNativeDateModule, MatSliderModule, MatRadioModule, MatSlideToggleModule, MatDatepickerModule} from '@angular/material';
 import { NpnSliderModule } from 'npn-slider';
@@ -26,6 +26,8 @@ import { faTwitter } from '@fortawesome/free-brands-svg-icons';
 import { faArrowAltRight, faLightbulb, faHeading, faLightbulbOn, faInfoCircle, } from '@fortawesome/pro-light-svg-icons';
 import { faPalette, faLayerGroup } from '@fortawesome/pro-solid-svg-icons';
 import {GatewayAdministrationWebsiteWidgetComponent} from './gateway-administration-website-widget/gateway-administration-website-widget.component';
+import {UnwrapTagDirective} from './_directives/unwrap-tag.directive';
+import {HrefPreventDefaultDirective} from './_directives/href-prevent-default.directive';
 // Add an icon to the library for convenient access in other components
 library.add(faCoffee, faStroopwafel, faCalendar, faHeading, faLayerGroup, faInfoCircle, faPalette, faTwitter, faArrowAltRight, faLightbulb, faLightbulbOn, faBraille, faBrush, faPaintBrush, faPaintRoller);
 /* Font awesome */
@@ -66,6 +68,8 @@ library.add(faCoffee, faStroopwafel, faCalendar, faHeading, faLayerGroup, faInfo
         NodeSmallDetailsWidgetComponent,
         FlotGraphComponent,
         NgSparklineDirective,
+        UnwrapTagDirective,
+        HrefPreventDefaultDirective,
         FullLeafletMapWidgetComponent,
         /*MapInstanceGetterComponent,*/
         GatewayAdministrationWidgetComponent,
